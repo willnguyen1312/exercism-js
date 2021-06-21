c06b3ab2e8dea18c39a64aa8e09094562f7f63ef
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-06-21 13:50:23 +0700

    Solve resistor-color 🎉

---
+++ src/resistor-color/resistor-color.js
@@ -0,0 +1,16 @@
+export const colorCode = (colorName) => {
+  return COLORS.indexOf(colorName);
+};
+
+export const COLORS = [
+  "black",
+  "brown",
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "violet",
+  "grey",
+  "white",
+];
