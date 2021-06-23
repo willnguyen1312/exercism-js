d27da2eea34b0ed546fefc8d2ded422ea62f4004
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-06-23 17:24:41 +0700

    Another toRoman

--- src/roman-numerals/roman-numerals.js
+++ src/roman-numerals/roman-numerals.js
@@ -1,16 +1,48 @@
-export const toRoman = (num) => {
-  let m = ["", "M", "MM", "MMM"];
-  let c = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"];
-  let x = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"];
-  let i = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
+// export const toRoman = (num) => {
+//   let m = ["", "M", "MM", "MMM"];
+//   let c = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"];
+//   let x = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"];
+//   let i = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
 
-  // Converting to roman
-  let thousands = m[Math.floor(num / 1000)];
-  let hundereds = c[Math.floor((num % 1000) / 100)];
-  let tens = x[Math.floor((num % 100) / 10)];
-  let ones = i[num % 10];
+//   // Converting to roman
+//   let thousands = m[Math.floor(num / 1000)];
+//   let hundereds = c[Math.floor((num % 1000) / 100)];
+//   let tens = x[Math.floor((num % 100) / 10)];
+//   let ones = i[num % 10];
 
-  let ans = thousands + hundereds + tens + ones;
+//   let ans = thousands + hundereds + tens + ones;
 
-  return ans;
+//   return ans;
+// };
+
+export const toRoman = (value) => {
+  let result = "";
+  let numbers = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000];
+  let sym = [
+    "I",
+    "IV",
+    "V",
+    "IX",
+    "X",
+    "XL",
+    "L",
+    "XC",
+    "C",
+    "CD",
+    "D",
+    "CM",
+    "M",
+  ];
+
+  let index = sym.length - 1;
+  while (value > 0) {
+    let div = Math.floor(value / numbers[index]);
+    value = value % numbers[index];
+    while (div--) {
+      result = result + sym[index];
+    }
+    index--;
+  }
+
+  return result;
 };
