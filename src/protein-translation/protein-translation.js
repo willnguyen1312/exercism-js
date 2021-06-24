e4cc1b2ab85df4aec774151488ee1dc6dd47243e
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-06-24 19:33:25 +0700

    Solve translate

---
+++ src/protein-translation/protein-translation.js
@@ -0,0 +1,25 @@
+const STOP = "STOP";
+
+const invert = (obj) =>
+  Object.fromEntries(
+    Object.entries(obj).flatMap(([protein, codons]) =>
+      codons.map((codon) => [codon, protein])
+    )
+  );
+
+const TRANSLATIONS = invert({
+  Methionine: ["AUG"],
+  Phenylalanine: ["UUU", "UUC"],
+  Leucine: ["UUA", "UUG"],
+  Serine: ["UCU", "UCC", "UCA", "UCG"],
+  Tyrosine: ["UAU", "UAC"],
+  Cysteine: ["UGU", "UGC"],
+  Tryptophan: ["UGG"],
+  [STOP]: ["", "UAA", "UAG", "UGA"],
+});
+
+export const translate = (rna = "") => {
+  const protein = TRANSLATIONS[rna.slice(0, 3)];
+  if (!protein) throw new Error("Invalid codon");
+  return protein === STOP ? [] : [protein, ...translate(rna.slice(3))];
+};
