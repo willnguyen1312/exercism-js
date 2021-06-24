35bcbc404ce0e122659ecdbd9afd2e64e7bd2182
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-06-24 18:36:58 +0700

    Rock

--- src/clock/clock.js
+++ src/clock/clock.js
@@ -3,10 +3,6 @@ export class Clock {
   static MINUTES_IN_HOUR = 60;
   static MINUTES_IN_DAY = Clock.MINUTES_IN_HOUR * Clock.HOURS_IN_DAY;
 
-  /**
-   * This static method converts a given number of minutes into a time of day expressed in minutes
-   * e.g. -20 minutes = 1420 minutes which is 23:40
-   */
   static timeInMinutes(minutes) {
     return (
       ((minutes % Clock.MINUTES_IN_DAY) + Clock.MINUTES_IN_DAY) %
@@ -16,10 +12,6 @@ export class Clock {
 
   #time = 0;
 
-  /**
-   * Converts given hours and minutes into total minutes and adds it to #time.
-   * #time is made private so the only way it can be manipulated is through the `plus` and `minus` instance methods.
-   */
   constructor(hours = 0, minutes = 0) {
     this.plus(hours * Clock.MINUTES_IN_HOUR + minutes);
   }
