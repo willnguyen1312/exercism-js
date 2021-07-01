e7d39c3ae1d7475fba5ba6c3e04a010d39cdd1a4
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-07-01 14:22:47 +0700

    Remove  comment

--- src/allergies/allergies.js
+++ src/allergies/allergies.js
@@ -12,18 +12,6 @@ const ALLERGENS = [
 export class Allergies {
   allergens = [];
 
-  /**
-   * Each allergen is assigned to an index such that 2^index = allergen score.
-   * For example:
-   * 2^3 === 8 === strawberries score
-   * This allows us to take the allergies score, convert it to a binary number, and know which allergies are present
-   * based on which digits are 1.
-   * For example:
-   * Allergies score = 53
-   * 53 in binary = 110101
-   * From right, 0, 2, 4, and 5 places have a 1
-   * So allergies = [eggs, shellfish, tomatoes, chocolate]
-   */
   constructor(score = 0) {
     this.allergens = ALLERGENS.filter((_, i) => (score >> i) & 1);
   }
